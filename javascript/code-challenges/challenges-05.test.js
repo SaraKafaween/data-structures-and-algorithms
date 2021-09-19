98f5fdbdfa60bb8f60de926676423ae5a33a4e9c
sara kafaween <sarakafaween@yahoo.com>
2021-09-19 03:00:22 +0300

    code055

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -11,7 +11,7 @@ should convert to ["Jane Doe", "James Bond"]
 Note the space in between first and last names.
 You can assume that neither firstName nor lastName will be blank
 ------------------------------------------------------------------------------------------------ */
-const toLastNames = (people) => {
+const toLastNames = people => {
   let newArr = people.map((element) => {
     return element.firstName + ' ' + element.lastName;
   });
